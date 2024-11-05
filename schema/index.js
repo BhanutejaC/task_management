e3fba85046f910f418ddafbd67c073b65444254a
bhanutejaC <chinthabhanuteja.cs22@bitsathy.ac.in>
2024-11-05 12:12:55 +0530

    schema changes

--- schema/index.js
+++ schema/index.js
@@ -1,10 +1,12 @@
 const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID, GraphQLList, GraphQLNonNull } = require('graphql');
 const Task = require('../models/Task');
 
+// Update the TaskType to include taskId
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: () => ({
         id: { type: GraphQLID },
+        taskId: { type: GraphQLString },  // New field for custom task identifier
         title: { type: GraphQLString },
         description: { type: GraphQLString },
         status: { type: GraphQLString },
@@ -12,6 +14,7 @@ const TaskType = new GraphQLObjectType({
     }),
 });
 
+// Update the RootQuery to include queries for taskId starting with '01'
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -28,22 +31,34 @@ const RootQuery = new GraphQLObjectType({
                 return Task.find();
             },
         },
+        tasksWithPrefix: {  // New query to get tasks with taskId starting with '01'
+            type: new GraphQLList(TaskType),
+            args: { prefix: { type: GraphQLString } },
+            resolve(parent, args) {
+                return Task.find({ taskId: { $regex: '^' + args.prefix } }); // Filtering by prefix
+            },
+        },
     },
 });
 
+// Update the Mutation to include taskId when adding tasks
 const Mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
         addTask: {
             type: TaskType,
             args: {
+                taskId: { type: new GraphQLNonNull(GraphQLString) },  // Custom task ID
                 title: { type: new GraphQLNonNull(GraphQLString) },
                 description: { type: GraphQLString },
+                status: { type: GraphQLString },
             },
             resolve(parent, args) {
                 const task = new Task({
+                    taskId: args.taskId,  // Include taskId when saving
                     title: args.title,
                     description: args.description,
+                    status: args.status,
                 });
                 return task.save();
             },
