deb6077621aee6d453a9015a062c02eaaea78f1d
bhanutejaC <chinthabhanuteja.cs22@bitsathy.ac.in>
2024-11-05 12:24:18 +0530

    spacing and delete feature

--- schema/index.js
+++ schema/index.js
@@ -1,85 +1,100 @@
-const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLID, GraphQLList, GraphQLNonNull, GraphQLFloat } = require('graphql');
-const Product = require('../models/Product'); // Import the Product model
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLFloat,
+} = require("graphql");
+const Product = require("../models/Product"); // Import the Product model
 
 // Define the ProductType
 const ProductType = new GraphQLObjectType({
-    name: 'Product',
-    fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        description: { type: GraphQLString },
-        price: { type: GraphQLFloat },
-        category: { type: GraphQLString },
-    }),
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    price: { type: GraphQLFloat },
+    category: { type: GraphQLString },
+  }),
 });
 
 // Define the Root Query
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        product: {
-            type: ProductType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args) {
-                return Product.findById(args.id);
-            },
-        },
-        products: {
-            type: new GraphQLList(ProductType),
-            resolve() {
-                return Product.find();
-            },
-        },
+  name: "RootQueryType",
+  fields: {
+    product: {
+      type: ProductType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Product.findById(args.id);
+      },
     },
+    products: {
+      type: new GraphQLList(ProductType),
+      resolve() {
+        return Product.find();
+      },
+    },
+  },
 });
 
 // Define the Mutations
 const Mutation = new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-        addProduct: {
-            type: ProductType,
-            args: {
-                name: { type: new GraphQLNonNull(GraphQLString) },
-                description: { type: new GraphQLNonNull(GraphQLString) },
-                price: { type: new GraphQLNonNull(GraphQLFloat) },
-                category: { type: new GraphQLNonNull(GraphQLString) },
-            },
-            resolve(parent, args) {
-                const product = new Product({
-                    name: args.name,
-                    description: args.description,
-                    price: args.price,
-                    category: args.category,
-                });
-                return product.save();
-            },
-        },
-        deleteProduct: {
-            type: ProductType,
-            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-            resolve(parent, args) {
-                return Product.findByIdAndRemove(args.id);
-            },
-        },
-        updateProduct: {
-            type: ProductType,
-            args: {
-                id: { type: new GraphQLNonNull(GraphQLID) },
-                name: { type: GraphQLString },
-                description: { type: GraphQLString },
-                price: { type: GraphQLFloat },
-                category: { type: GraphQLString },
-            },
-            resolve(parent, args) {
-                return Product.findByIdAndUpdate(args.id, args, { new: true });
-            },
-        },
+  name: "Mutation",
+  fields: {
+    addProduct: {
+      type: ProductType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        price: { type: new GraphQLNonNull(GraphQLFloat) },
+        category: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const product = new Product({
+          name: args.name,
+          description: args.description,
+          price: args.price,
+          category: args.category,
+        });
+        return product.save();
+      },
     },
+    deleteProduct: {
+      type: ProductType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Product.findByIdAndRemove(args.id);
+      },
+    },
+    updateProduct: {
+      type: ProductType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        price: { type: GraphQLFloat },
+        category: { type: GraphQLString },
+      },
+      deleteProduct: {
+        type: ProductType,
+        args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+        resolve(parent, args) {
+          return Product.findByIdAndDelete(args.id); // Change to findByIdAndDelete
+        },
+      },
+      resolve(parent, args) {
+        return Product.findByIdAndUpdate(args.id, args, { new: true });
+      },
+    },
+  },
 });
 
 // Export the schema
 module.exports = new GraphQLSchema({
-    query: RootQuery,
-    mutation: Mutation,
+  query: RootQuery,
+  mutation: Mutation,
 });
